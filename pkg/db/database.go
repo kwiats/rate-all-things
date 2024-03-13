87238f369d6d8750957aa4d750eea64f094a55dd
kwiats <pawuskwiatkowski@gmail.com>
2024-03-13 08:59:41 +0100

    [main] - added models to migrations

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -1,26 +1,34 @@
 package database
 
 import (
+	"log"
+
 	"github.com/kwiats/rate-all-things/pkg/model"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func InitializeDB() (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open("rat.db"), &gorm.Config{})
+func InitializeDB(connectionUri string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: connectionUri}), &gorm.Config{
+		//Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		return nil, err
 	}
 
 	err = db.AutoMigrate(
 		&model.Review{},
+		&model.ReviewItemData{},
 		&model.Category{},
 		&model.CustomField{},
 		&model.CategoryCustomField{},
+		&model.User{},
 	)
 	if err != nil {
+		log.Printf("Failed to auto-migrate database models: %v\n", err)
 		return nil, err
 	}
 
+	log.Println("Database initialized successfully")
 	return db, nil
 }
