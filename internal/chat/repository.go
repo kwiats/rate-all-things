c66fca7c4e62e2b08fbc7add2f6205f60bce5606
kwiats <pawuskwiatkowski@gmail.com>
2024-04-09 14:50:50 +0200

    [main] - added repository layer for chat

--- internal/chat/repository.go
+++ internal/chat/repository.go
@@ -1,6 +1,7 @@
 package chat
 
 import (
+	"time"
 	"tit/internal/db/model"
 
 	"gorm.io/gorm"
@@ -20,6 +21,26 @@ func (repo *ChatRepository) CreateChat(chat *model.Chat) (*model.Chat, error) {
 	}
 	return chat, nil
 }
+func (r *ChatRepository) GetUserChats(userID uint) ([]*model.Chat, error) {
+	var chats []*model.Chat
+
+	err := r.db.Preload("Users").Joins("JOIN chat_users on chat_users.chat_id = chats.id").Where("chat_users.user_id = ?", userID).Find(&chats).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return chats, nil
+}
+func (r *ChatRepository) GetLastMessage(chatID uint) (*model.Message, error) {
+	var message model.Message
+
+	err := r.db.Where("chat_id = ?", chatID).Order("created_at DESC").First(&message).Error
+	if err != nil {
+		return &model.Message{}, err
+	}
+
+	return &message, nil
+}
 
 func (repo *ChatRepository) GetChatByID(id uint) (*model.Chat, error) {
 	var chat model.Chat
@@ -28,6 +49,68 @@ func (repo *ChatRepository) GetChatByID(id uint) (*model.Chat, error) {
 		return nil, result.Error
 	}
 	return &chat, nil
+}
+func (repo *ChatRepository) SignAsReadBy(messageID uint, userID uint) (bool, error) {
+	var exists int64
+	repo.db.Model(&model.MessageRead{}).Where("message_id = ? AND user_id = ?", messageID, userID).Count(&exists)
+	if exists > 0 {
+		return true, nil
+	}
+
+	messageRead := model.MessageRead{
+		UserID:    userID,
+		MessageID: messageID,
+		ReadAt:    time.Now(),
+	}
+
+	if err := repo.db.Create(&messageRead).Error; err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (repo *ChatRepository) GetMessagesFromChat(chatID uint, filters QueryParamsFilters) ([]model.Message, error) {
+	query := repo.db.Preload("MessageReads").Where("chat_id = ?", chatID)
+
+	if filters.UserID != 0 {
+		query = query.Joins("JOIN message_reads ON message_reads.message_id = messages.id").
+			Where("message_reads.user_id = ?", filters.UserID)
+	}
+
+	if filters.SenderID != 0 {
+		query = query.Where("sender_id = ?", filters.SenderID)
+	}
+
+	if filters.StartDate != "" {
+		query = query.Where("created_at >= ?", filters.StartDate)
+	}
+	if filters.EndDate != "" {
+		query = query.Where("created_at <= ?", filters.EndDate)
+	}
+
+	if filters.ContentLike != "" {
+		query = query.Where("content LIKE ?", "%"+filters.ContentLike+"%")
+	}
+
+	if filters.PageSize > 0 {
+		offset := (filters.Page - 1) * filters.PageSize
+		query = query.Offset(offset).Limit(filters.PageSize)
+	}
+
+	if filters.OrderBy != "" {
+		query = query.Order(filters.OrderBy)
+	} else {
+		query = query.Order("created_at desc")
+	}
+
+	var messages []model.Message
+	err := query.Find(&messages).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
 }
 
 func (repo *ChatRepository) StoreMessage(message *model.Message) (*model.Message, error) {
