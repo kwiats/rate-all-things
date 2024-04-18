d3315d76a8e4a9d1743ac70745cdc8e00de7ffdf
kwiats <pawuskwiatkowski@gmail.com>
2024-04-18 13:45:23 +0200

    [main] - added config

--- internal/config/configurations.go
+++ internal/config/configurations.go
@@ -3,19 +3,28 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Environment struct {
-	Host      string
-	User      string
-	Password  string
-	DBName    string
-	Port      string
-	SslMode   string
-	TimeZone  string
-	SecretKey string
+	Host                 string
+	User                 string
+	Password             string
+	DBName               string
+	Port                 string
+	SslMode              string
+	TimeZone             string
+	SecretKey            string
+	MinioHost            string
+	MinioRootUser        string
+	MinioRootPassword    string
+	MinioDefaultBucket   string
+	MinioAccessKey       string
+	MinioSecretAccessKey string
+	MinioIsSecure        bool
+	AllowedDomains       string
 }
 
 type Config struct {
@@ -31,15 +40,24 @@ func NewConfiguration() (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	envs := Environment{
-		Host:      os.Getenv("POSTGRES_HOST"),
-		User:      os.Getenv("POSTGRES_USER"),
-		Password:  os.Getenv("POSTGRES_PASSWORD"),
-		DBName:    os.Getenv("POSTGRES_NAME"),
-		Port:      os.Getenv("POSTGRES_PORT"),
-		SslMode:   os.Getenv("SSLMODE"),
-		TimeZone:  os.Getenv("TIMEZONE"),
-		SecretKey: os.Getenv("SECRET_KEY"),
+		Host:                 GetValue("POSTGRES_HOST", ""),
+		User:                 GetValue("POSTGRES_USER", ""),
+		Password:             GetValue("POSTGRES_PASSWORD", ""),
+		DBName:               GetValue("POSTGRES_NAME", ""),
+		Port:                 GetValue("POSTGRES_PORT", ""),
+		SslMode:              GetValue("SSLMODE", ""),
+		TimeZone:             GetValue("TIMEZONE", ""),
+		SecretKey:            GetValue("SECRET_KEY", ""),
+		MinioHost:            GetValue("MINIO_HOST", ""),
+		MinioRootUser:        GetValue("MINIO_ROOT_USER", ""),
+		MinioRootPassword:    GetValue("MINIO_ROOT_PASSWORD", ""),
+		MinioDefaultBucket:   GetValue("MINIO_DEFAULT_BUCKETS", ""),
+		MinioAccessKey:       GetValue("MINIO_ACCESS_KEY", ""),
+		MinioSecretAccessKey: GetValue("MINIO_SECRET_ACCESS_KEY", ""),
+		AllowedDomains:       GetValue("ALLOWED_DOMAINS", ""),
+		MinioIsSecure:        GetBool("MINIO_IS_SECURE", false),
 	}
 
 	return &Config{Envs: envs}, nil
@@ -52,4 +70,21 @@ func (c *Config) GetDBConnectionUri() string {
 
 func (c *Config) GetSecretKey() string {
 	return c.Envs.SecretKey
+}
+
+func GetBool(key string, defaultValue bool) bool {
+	value_from_env := os.Getenv(key)
+	value, err := strconv.ParseBool(value_from_env)
+	if err != nil {
+		value = defaultValue
+	}
+	return value
+}
+
+func GetValue(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
