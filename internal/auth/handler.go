fc5ad7cf4276c2c80918820033e793c9a36773d6
kwiats <pawuskwiatkowski@gmail.com>
2024-04-24 19:11:36 +0200

    [main] - added register for handler

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -29,3 +29,31 @@ func HandleLogin(authService *AuthService) http.HandlerFunc {
 
 	}
 }
+
+func HandleRegister(authService *AuthService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var signInDTO SignInDTO
+
+		if err := r.ParseMultipartForm(10 << 20); err != nil {
+			http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+			log.Printf("Error parsing form data: %v\n", err)
+			return
+		}
+
+		signInDTO.Username = r.FormValue("username")
+		signInDTO.Password = r.FormValue("password")
+		signInDTO.Email = r.FormValue("email")
+		if signInDTO.Username == "" || signInDTO.Password == "" || signInDTO.Email == "" {
+			common.WriteJSON(w, http.StatusBadRequest, "Missing username, password, or email")
+			return
+		}
+		response, err := authService.RegisterUser(signInDTO)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Printf("User not created: %v\n", err)
+			return
+		}
+		common.WriteJSON(w, http.StatusAccepted, response)
+
+	}
+}
