a40f8c25b37a4ab1421b6c645fc99ea915acc754
kwiats <pawuskwiatkowski@gmail.com>
2024-04-29 08:39:24 +0200

    [main] - imporved dto for login, register and user

--- internal/auth/dto.go
+++ internal/auth/dto.go
@@ -1,20 +1,32 @@
 package auth
 
+import "mime/multipart"
+
 type LoginUserDTO struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 type SignInDTO struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	FirstName      string                `json:"firstname"`
+	LastName       string                `json:"lastname"`
+	ProfilePicture *multipart.FileHeader `json:"-"`
+	Username       string                `json:"username"`
+	Email          string                `json:"email"`
+	Password       string                `json:"password"`
 }
 type UserDTO struct {
 	Id             uint   `json:"id"`
 	Username       string `json:"username"`
+	FirstName      string `json:"firstname"`
+	LastName       string `json:"lastname"`
 	ProfilePicture string `json:"profile_picture,omitempty"`
 	Email          string `json:"email,omitempty"`
+}
+
+type LoginDTO struct {
+	Token string  `json:"token"`
+	User  UserDTO `json:"user"`
 }
 
 type Token struct {
