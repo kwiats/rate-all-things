3fac9cc47062305cfeeda63b494609cfaa18988e
kwiats <pawuskwiatkowski@gmail.com>
2024-05-01 08:40:54 +0200

    [main] - fixed service for fileHeader

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"tit/internal/db/model"
+	"tit/pkg/media"
 	"tit/pkg/util"
 )
 
 type AuthService struct {
 	repository IAuthRepository
+	storage    *media.MediaService
 }
 
 type IAuthRepository interface {
@@ -16,32 +18,37 @@ type IAuthRepository interface {
 	SignIn(*model.User) (*model.User, error)
 }
 
-func NewAuthService(repository IAuthRepository) *AuthService {
-	return &AuthService{repository: repository}
+func NewAuthService(repository IAuthRepository, mediaStorage *media.MediaService) *AuthService {
+	return &AuthService{repository: repository, storage: mediaStorage}
 }
 
-func (service *AuthService) LoginUser(user LoginUserDTO) (string, error) {
-
+func (service *AuthService) LoginUser(user LoginUserDTO) (*LoginDTO, error) {
 	userFromDB, err := service.repository.GetUserByUsername(user.Username)
 	if err != nil {
-		return "", errors.New("cannot found user")
+		// Wrap the error to maintain the underlying error context
+		return nil, fmt.Errorf("user not found: %w", err)
 	}
 
 	isLogged := util.VerifyUserPassword(user.Password, []byte(userFromDB.Password))
 	if !isLogged {
-		return "", errors.New("invalid credentials")
+		return nil, errors.New("invalid credentials")
 	}
 
 	userDTO := UserDTO{
-		Id:       userFromDB.ID,
-		Username: userFromDB.Username,
-	}
-	accessToken, err := NewAccessToken(userDTO)
-	if err != nil {
-		return "", fmt.Errorf("failed to generate access token: %w", err)
+		Id:             userFromDB.ID,
+		Username:       userFromDB.Username,
+		FirstName:      userFromDB.FirstName,
+		LastName:       userFromDB.LastName,
+		ProfilePicture: userFromDB.ProfilePicture,
 	}
 
-	return accessToken, nil
+	accessToken, err := NewAccessToken(userDTO)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate access token: %w", err)
+	}
+
+	loginDTO := &LoginDTO{Token: accessToken, User: userDTO}
+	return loginDTO, nil
 }
 
 func (service *AuthService) RegisterUser(userDTO SignInDTO) (*UserDTO, error) {
@@ -49,20 +56,34 @@ func (service *AuthService) RegisterUser(userDTO SignInDTO) (*UserDTO, error) {
 	if err != nil {
 		return &UserDTO{}, err
 	}
-	
+	var profilePicturePath string
+	if userDTO.ProfilePicture != nil {
+		profilePicturePath, err = service.storage.SaveFile(userDTO.ProfilePicture, userDTO.Username)
+		if err != nil {
+			return nil, fmt.Errorf("error saving profile picture: %w", err)
+		}
+	}
+
 	userDAO := model.User{
-		Username: userDTO.Username,
-		Email:    userDTO.Email,
-		Password: string(*hashedPassword),
+		FirstName:      userDTO.FirstName,
+		LastName:       userDTO.LastName,
+		ProfilePicture: profilePicturePath,
+		Username:       userDTO.Username,
+		Email:          userDTO.Email,
+		Password:       string(*hashedPassword),
 	}
 
 	user, err := service.repository.SignIn(&userDAO)
 	if err != nil {
 		return &UserDTO{}, err
 	}
+
 	return &UserDTO{
-		Id:       user.ID,
-		Username: user.Username,
-		Email:    user.Email,
+		Id:             user.ID,
+		Username:       user.Username,
+		FirstName:      user.FirstName,
+		LastName:       user.LastName,
+		Email:          user.Email,
+		ProfilePicture: user.ProfilePicture,
 	}, nil
 }
