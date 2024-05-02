f9405c21fcd914a1988a99663b1e214b03419234
kwiats <pawuskwiatkowski@gmail.com>
2024-05-02 08:58:20 +0200

    [main] - added fix for routing

--- internal/auth/router.go
+++ internal/auth/router.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"sync"
+	"tit/pkg/media"
 
 	"github.com/gorilla/mux"
 
@@ -12,8 +13,11 @@ func HandleAuthRouter(db *gorm.DB, r *mux.Router, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	router := r.PathPrefix("/api/auth").Subrouter()
+	minioStorage, _ := media.NewMinioBlobStorage("minio:9000", "bqvayHlLY5pHdimH", "zthpQFaAnbVvUwrobLCA0M3jVc6U9mdl", false, "pictures")
+	mediaService := media.NewMediaService(minioStorage)
+
 	authRepo := NewAuthRepository(db)
-	authService := NewAuthService(authRepo)
+	authService := NewAuthService(authRepo, mediaService)
 
 	router.HandleFunc("/login", HandleLogin(authService)).Methods("POST")
 	router.HandleFunc("/register", HandleRegister(authService)).Methods("POST")
